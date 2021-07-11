f162281c8824bdd25bb2cf26528fb922ac5c6254
Jeremy <77142843+JeremyLee-github@users.noreply.github.com>
2021-07-11 16:35:50 +0800

    Add files via upload

--- 線上學習平台學習數據分析與即時推薦系統/batch/BatchAnalysis.scala
+++ 線上學習平台學習數據分析與即時推薦系統/batch/BatchAnalysis.scala
@@ -22,10 +22,12 @@ object BatchAnalysis {
     val properties: Properties = new Properties()
     properties.setProperty("user","root")
     properties.setProperty("password","root")
-    val allInfoDF: Dataset[AnswerWithRecommendations] = spark.read.jdbc(
+    val allInfoDF: Dataset[AnswerWithRecommendations] = spark
+      .read // 離線分析
+      .jdbc(
       "jdbc:mysql://localhost:3306/edu?useUnicode=true&characterEncoding=utf8",
       "t_recommended",
-      properties).as[AnswerWithRecommendations] //轉為.as[AnswerWithRecommendations]範型資料格式(最後一項帶有預測推薦題目!!!)
+      properties).as[AnswerWithRecommendations] //轉為.as[AnswerWithRecommendations]範型資料格式(最後一項帶有Recommendations預測推薦題目!!!)
 
     //處理分析數據
     //需求一:各科目熱門題目分析(找到TOP50熱門題目,然後統計這些科目中,分別包含這幾個熱門題目的條目數)
@@ -59,22 +61,47 @@ object BatchAnalysis {
       .orderBy('hotCount.desc)
 
     //需求二:各科目推薦題目分析 (找到TOP20熱門題目對應的推薦題目,然後找到推薦題目對應的科目,並統計每個科目分別包含推薦題目的個數)
+    /*
+    題號 熱度
+    1    10
+    2     9
+    題號  熱度  推薦題
+    1    10   2,3,4
+    2     9   3,4,5
+    推薦題   科目
+    2       數學
+    3       數學
+    4       物理
+    5       化學
+    科目   推薦題目數量
+    數學     5
+    數學     4
+    物理     3
+    化學     1
+     */
     //TODO ===DSL
     //1.統計TOP20熱門題目--子查詢t1
-    val hotTOP20: Dataset[Row] = allInfoDF.groupBy('question_id).agg(count('question_id) as "hot_question_count")
-      .orderBy('hot_question_count.desc).limit(20)
+    val hotTOP20: Dataset[Row] = allInfoDF
+      .groupBy('question_id)
+      .agg(count('question_id) as "hot_question_count")
+      .orderBy('hot_question_count.desc)
+      .limit(20)
     //2.將t1和原始列表t_answer關聯,得到TOP20熱門題目的推薦列表t2
-    val ridAndsDS: DataFrame = hotTOP20.join(allInfoDF.dropDuplicates("question_id"),'question_id)
+    val ridAndsDS: DataFrame = hotTOP20
+      .join(allInfoDF.dropDuplicates("question_id"),'question_id)
       .select("recommendations")
-    //3.用Split將recommendations中的ids用","切分為數組,然後用explode將列轉為行,並記為t3
-    val ridsDS: Dataset[Row] = ridAndsDS.select(explode(split('recommendations, ",") as "question_id"))
+    //3.用Split將recommendations中的ids用","切分為數組,然後用explode將列轉為行,並記為t3,.dropDuplicates("rid") => 進行去重
+    val ridsDS: Dataset[Row] = ridAndsDS
+      .select(explode(split('recommendations, ",") as "rid"))
       .dropDuplicates("rid")
     //4.對推薦題目進行去重,將t3和t_answer原始表進行join,得到每個推薦題目所屬的科目,記為t4
     //df1.join(df2, $"df1key"===$"df2key")  or  df1.join(df2).where($"df1key"===$"df2key") =>源碼join兩種寫法
     val ridsAndSid: DataFrame = ridsDS.join(allInfoDF.dropDuplicates("question_id"), "question_id")
     //5.統計各個科目包含的推薦題目數量並倒序排列(已經去重)
-    val result2: Dataset[Row] = ridsAndSid.groupBy("subject_id")
-      .agg(count("*") as "rcount").orderBy('rcount.desc)
+    val result2: Dataset[Row] = ridsAndSid
+      .groupBy("subject_id")
+      .agg(count("*") as "rcount")
+      .orderBy('rcount.desc)
 
 
     //輸出結果
